7ae2d7e162258eb0956ef9cb60298e9e364f5426
JaehoLee <chosloo2835@gmail.com>
2024-03-12 17:35:34 +0900

    update : 동적 메타 태그 설정 추가

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,29 +1,30 @@
 import { Locale } from "@/i18n";
 import getTranslation from "@/lib/i18n/getTranslation";
 import Home from "@/components/Home";
+import type { Metadata } from "next";
 
 type Props = {
   params: { locale: Locale };
 };
 
+export const dynamic = "force-dynamic";
+
+// 동적 메타 태그 설정
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const translation = await getTranslation(params.locale);
+
+  return {
+    title: translation("meta.title"),
+    description: translation("meta.description"),
+  };
+}
+
 export default async function HomePage({ params }: Props) {
   const translation = await getTranslation(params.locale);
 
   return (
-    <section className="flex flex-col items-center mt-8 gap-7">
-      <div>
-        <Home translation={translation} />
-      </div>
-      <div>
-        <div className="">
-          <input
-            type="file"
-            accept="image/*"
-            className="border rounded-lg px-4 py-2"
-          />
-          <div className="mt-4"></div>
-        </div>
-      </div>
+    <section className="flex flex-col items-center">
+      <Home translation={translation} />
     </section>
   );
 }
