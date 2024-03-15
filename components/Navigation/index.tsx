e910b19aa747df22c0432b2e0da5eefc7833ff62
JaehoLee <chosloo2835@gmail.com>
2024-03-15 12:04:04 +0900

    public 추가

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -9,21 +9,17 @@ interface Props {
 export default function Nav({ translation }: Props) {
   return (
     <nav className="absolute h-16 w-full flex items-center justify-between border-b border-b-neutral-200 bg-white px-8 lg:px-48">
-      {/* Logo */}
       <div className="flex items-center">
         <Logo />
       </div>
 
-      {/* Menu and Locale Selector */}
       <div className="flex items-center space-x-4">
-        {/* Menu */}
         <div className="hidden md:flex space-x-4">
           <MenuItem href="/" text={translation("nav.menu.home")} />
           <MenuItem href="/about" text={translation("nav.menu.about")} />
           <MenuItem href="/contact" text={translation("nav.menu.contact")} />
         </div>
 
-        {/* Locale Selector */}
         <LocaleSelector message={translation("nav.locale-selector.message")} />
       </div>
     </nav>
